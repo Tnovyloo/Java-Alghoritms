f893504b3af11b6e621894af7e0cf1f69ea098ec
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-11-10 23:04:53 +0100

    Solving paths problem and also ATM machine

--- a19/a19/src/A19AZ.java
+++ a19/a19/src/A19AZ.java
@@ -1,3 +1,61 @@
 public class A19AZ {
-    
+    static int[][] graph = {
+            {0, 300, 402, 356, -1, -1, -1, -1, -1},
+            {300, 0, -1, -1, 440, 474, -1, -1, -1},
+            {402, -1, 0, -1, -1, 330, -1, -1, -1},
+            {356, -1, -1, 0, -1, -1, 823, -1, -1},
+            {-1, 440, -1, -1, 0, -1, -1, 430, -1},
+            {474, 330, -1, 823, -1, 0, 813, 365, -1},
+            {-1, -1, -1, 823, -1, 813, 0, -1, 403},
+            {-1, -1, -1, -1, 430, 365, -1, 0, 768},
+            {-1, -1, -1, -1, -1, 774, 403, 768, 0}
+    };
+
+    static String[] cities = {
+            "Warszawa",
+            "Katowice",
+            "Zakopane",
+            "Lwów",
+            "Wiedeń",
+            "Budapeszt",
+            "Bukareszt",
+            "Zagreb",
+            "Sofia"
+    };
+
+    public static void shortestPath(int start, int end) {
+        boolean[] visited = new boolean[graph.length];
+        int currentCity = start;
+        int totalDistance = 0;
+
+        visited[currentCity] = true;
+        System.out.println("Trasa: " + cities[currentCity]);
+
+        while(currentCity != end) {
+            int nextCity = -1;
+            int shortestDistance = Integer.MAX_VALUE;
+
+            for(int i = 0; i < graph[currentCity].length; i++) {
+                if(!visited[i] && graph[currentCity][i] != -1 && graph[currentCity][i] < shortestDistance) {
+                    shortestDistance = graph[currentCity][i];
+                    nextCity = i;
+                }
+            }
+
+            if(nextCity == -1) {
+                System.out.println("Nie znaleziono trasy.");
+                return;
+            }
+
+            totalDistance += shortestDistance;
+            visited[nextCity] = true;
+            currentCity = nextCity;
+            System.out.println("-> " + cities[nextCity] + " odległość: " + shortestDistance + " km.");
+        }
+        System.out.println("Calkowita odleglosc: " + totalDistance + " km.");
+    }
+
+    public static void main(String[] args) {
+        shortestPath(0, 8);
+    }
 }
--- a28/a28/src/A28AZ.java
+++ a28/a28/src/A28AZ.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class A28AZ {
+    static final int[] NOMINALS = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+    static final int[] LIMITS = {1, 2, 0, 7, 2, 2, 5, 2, 10};
     
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe w groszach: ");
+
+        int sum = 0;
+        int[] reserve = LIMITS;
+        ArrayList<Integer> usedNominals = new ArrayList<>();
+
+        int input = scanner.nextInt();
+        scanner.close();
+
+        while (sum < input) {
+            for (int i = 0; i < NOMINALS.length; i++) {
+                if (reserve[i] > 0 && NOMINALS[i] + sum <= input) {
+                    usedNominals.add(NOMINALS[i]);
+                    reserve[i]--;
+                    sum += NOMINALS[i];
+                    i--;
+                }
+            }
+            System.out.println(usedNominals);
+            for (Integer integer : reserve) {
+                System.out.print(integer + ", ");
+            }
+
+            break;
+        }
+
+
+    }
 }
